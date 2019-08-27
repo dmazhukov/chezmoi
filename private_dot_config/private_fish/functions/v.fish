ab3d0ac25abe579561c5f1275ac88677f82064ce
Dmitriy Zhukov <dmitry0983@gmail.com>
2019-08-27 13:50:24 +0300

    shortcuts to view vars and files

---
+++ private_dot_config/private_fish/functions/v.fish
@@ -0,0 +1,4 @@
+# Defined in /var/folders/v2/2dz8rn_d3wn6fr74v9btvkj00000gn/T//fish.wj0QQp/v.fish @ line 1
+function v
+	hls $argv
+end
---
+++ private_dot_config/private_fish/functions/vv.fish
@@ -0,0 +1,7 @@
+# Defined in /var/folders/v2/2dz8rn_d3wn6fr74v9btvkj00000gn/T//fish.53Fp29/vv.fish @ line 2
+function vv --description 'View variable'
+	echo.BoldULYellow 'set -S'
+    set -S $argv | hls -S fish
+    echo.BoldULYellow 'echo'
+    eval 'echo $'$argv | hls -S fish
+end
